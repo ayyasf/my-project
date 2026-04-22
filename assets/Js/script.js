04dfcc392c6d1d10427ed83743e713ef479210d6
ayyasf <haninayash@gmail.com>
2026-04-22 15:20:42 +0300

    fixed

---
+++ assets/Js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const bars = document.querySelector('.bars');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const closeBtn = document.getElementById('closeBtn');
+  const menuLinks = mobileMenu.querySelectorAll('a');
+  bars.addEventListener('click', () => mobileMenu.classList.add('open'));
+  closeBtn.addEventListener('click', () => mobileMenu.classList.remove('open'));
+  menuLinks.forEach(link => link.addEventListener('click', () => mobileMenu.classList.remove('open')));
+});
